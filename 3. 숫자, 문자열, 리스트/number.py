fc8e8e96b98ccb3e46c746bca194b6dafb1ad3e6
barcordland <barcordland@gmail.com>
2020-07-13 00:31:45 +0000

    추가

---
+++ 3. 숫자, 문자열, 리스트/number.py
@@ -0,0 +1,18 @@
+# 아래에 코드를 드래그해서 복사한 후에 오른쪽 옆에다가 붙여 넣어 보세요. 
+
+2 + 2
+
+50 - 5*6
+
+(50 - 5*6) / 4
+
+8 / 5  # division always returns a floating point number
+
+
+
+# 면적을 구해볼까요? 아래의 코드를 복사해서 오른쪽에 붙여 넣어 보세요. 
+
+width = 20
+height = 5 * 9
+width * height
+
