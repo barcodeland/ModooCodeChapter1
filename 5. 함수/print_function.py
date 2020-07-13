cbe35333b0320e07691954c570a4e121389f0b7e
barcordland <barcordland@gmail.com>
2020-07-13 11:46:28 +0000

    함수도 넣었다.

---
+++ 5. 함수/print_function.py
@@ -0,0 +1,29 @@
+# 나는 '이제'라는 말을 앞에 항상 붙이는 버릇이 있어. 
+
+# 그러면 할 말에다 항상 붙여주면 좋겠다. 
+halmal1 = "코딩이 정말 하고 싶었죠?"
+
+print("이제 " + halmal1)
+
+# 그래 이렇게 계속 똑같은 짓을 반복하면 내 삶이 윤택해지겠네
+halmal2 = "진짜로 할 수 있어요."
+
+print("이제 " + halmal2)
+
+# 인내심이 생겼다. 
+
+halmal3 = "진짜 못참겠다. "
+
+print("이제 " + halmal3)
+
+
+
+# 그렇다면 function이 출동하면 어떨까? 
+
+
+def ijaePrint(string) : 
+  print("이제 " + string) 
+
+ijaePrint("이렇게 쉬워졌다. ")
+ijaePrint("이것만 있으면")
+ijaePrint("하고싶은 말만 넣음되네")
